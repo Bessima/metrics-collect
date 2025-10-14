205cd4eab19a614f16af532207ad63c733f81dba
ansima <bessima13@yandex.ru>
2025-10-14 14:52:41 +0300

    Fixing processing response code from client

--- internal/agent/client.go
+++ internal/agent/client.go
@@ -9,17 +9,27 @@ import (
 
 const Domain = "http://localhost:8080"
 
-func SendMetric(typeMetric string, name string, value string) {
-	postURL := fmt.Sprintf("%s/update/%s/%s/%s", Domain, typeMetric, name, value)
-	response, err := http.Post(postURL, "text/plain; charset=utf-8", nil)
-	if err != nil {
-		log.Fatalf("Failed to create resource at: %s and the error is: %v\n", postURL, err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
+func SendMetric(typeMetric string, name string, value string) error {
+	client := &http.Client{}
 
+	postURL := fmt.Sprintf("%s/update/%s/%s/%s", Domain, typeMetric, name, value)
+	response, err := client.Post(postURL, "text/plain; charset=utf-8", nil)
+	if err != nil {
+		log.Printf("Failed to create resource at: %s and the error is: %v\n", postURL, err)
+		return err
+	}
+
+	defer func() {
+		if err := response.Body.Close(); err != nil {
+			log.Printf("Error closing response body: %v\n", err)
 		}
-	}(response.Body)
+	}()
+	if response.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(response.Body)
+		log.Printf("Server returned non-OK status: %d, body: %s\n", response.StatusCode, string(body))
+		return fmt.Errorf("server returned status: %d", response.StatusCode)
+	}
+
 	log.Println("Successful sending: ", postURL)
+	return nil
 }
